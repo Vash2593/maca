7545f90ceb73326054b2e4f0b2e4d2bd40459dc0
David Moreira <moreira@lrde.epita.fr>
2012-05-19 13:56:47 +0200

    Create destinaiton bdd. Clean.
    * src/maca.cc: here.

--- src/maca.cc
+++ src/maca.cc
@@ -1,5 +1,5 @@
 #include <fstream>
-#include <map>
+#include <vector>
 #include <cmath>
 #include <cassert>
 #include <bdd.h>
@@ -12,50 +12,71 @@ public:
   parse()
     : states_(bddfalse)
     , transition_(bddfalse)
-  {}
-
-  int parse_bdd(std::string str)
   {
     bdd_init(1000000, 10000);
     bdd_setvarnum(1000);
+  }
+
+  int parse_bdd(std::string str)
+  {
     kripke::driver d;
-    if (str == "-")
-      {
-        assert(!!false);
-      }
-    unsigned int size;
-    {
-      std::ifstream i;
-      i.open(str);
-      i >> size;
-      i.close();
-    }
 
-    unsigned int nb_bits = (unsigned int) std::ceil(std::log2(size));
+    const unsigned size = get_size(str);
+    const unsigned bits_need = get_bits_need(size);
 
-    std::map<int, bdd> m;
+    bddPair* pair = get_pair(bits_need);
+
+    std::vector<bdd> source;
+    std::vector<bdd> destination;
     for (unsigned i = 0; i < size; ++i)
       {
-        bdd acu = bddtrue;
+        bdd new_source = bddtrue;
         unsigned decu = i;
-        for (unsigned j = 32; j > 0; decu /= 2)
+        for (unsigned j = bits_need; j > 0; decu /= 2)
           {
             --j;
             if (decu & 1)
-              acu &= bdd_ithvar(j);
+                new_source &= bdd_ithvar(j);
             else
-              acu = acu & !bdd_ithvar(j);
+                new_source &= !bdd_ithvar(j);
           }
-        m[i] = acu;
-        std::cout << i << '\t' << acu << std::endl;
+        source[i] = new_source;
+        destination[i] = bdd_replace(new_source, pair);
       }
     return d.parse_file(str, states_, transition_);
   }
+private:
+  bddPair* get_pair(unsigned bits_need)
+  {
+    // Create the pair
+    bddPair* pair = bdd_newpair();
+    for (unsigned j = 0; j < bits_need; ++j)
+      if (!bdd_setpair(pair, j, j + bits_need))
+        assert(false);
+    return pair;
+  }
+  unsigned get_size(std::string& str)
+  {
+    if (str == "-")
+      assert(!!false);
+    unsigned int size;
 
+    std::ifstream i;
+    i.open(str);
+    i >> size;
+    i.close();
+
+    return size;
+  }
+  unsigned get_bits_need(unsigned size)
+  {
+    return (unsigned int) std::ceil(std::log2(size));
+  }
 private:
   bdd states_;
   bdd transition_;
 };
+
 int main(int argc, char** argv)
 {
   assert(argc == 2);
