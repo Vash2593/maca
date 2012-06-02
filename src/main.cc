a7b2700b914045fe95c588ec5088ae3d2a73034f
David Moreira <moreira@lrde.epita.fr>
2012-06-02 22:49:07 +0200

    Add the `verif' class in the main. Test simple command.
    * src/main.cc (main): here.

--- src/main.cc
+++ src/main.cc
@@ -1,10 +1,21 @@
 #include <parseKripke.hh>
+#include <verification/verification.hh>
 
 int main(int argc, char** argv)
 {
+  using bddmap = parseKripke::bddmap;
   assert(argc == 2);
   parseKripke k;
   k.parse_bdd(std::string(argv[1]));
+
+  verif::verif f(k.get_states(), k.get_transitions(),
+                 k.get_succs(), k.get_succ_to_pred(),
+                 k.get_pred_to_succ(), k.get_map());
+
+  bddmap& map = k.get_map();
+  bdd a = bdd_ithvar(map["a"]);
+
+  std::cerr << f.bdd_eglobally(f.bdd_ap(a)) << std::endl;
 
   return 0;
 }
